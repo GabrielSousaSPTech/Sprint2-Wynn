f9276bc724857f14ede09ba1f98ffef8a184157b
GabrielSousaSPTech <gabriel.sousa@sptech.school>
2024-09-21 10:38:39 -0300

    Implementação do sistema de ATA

---
+++ Documentação e backlog/Ata/Sistema/script.js
@@ -0,0 +1,39 @@
+function mostrarData(){
+    const data = input_data.value;
+    mostrar_data.innerHTML = `<h2>${data.split('-').reverse().join('-')}</h2>`
+}
+
+var index1 = 1;
+
+function adicionarTema(){
+const tema = input_tema.value;
+const texto = input_texto.value;
+
+temas.innerHTML += `<p id="tema_${index1}"> • <strong>${tema}</strong>: ${texto} <button class="botao_apagar" onclick="apagarTema(${index1})">x</button></p></p>`  
+
+index1++
+}
+
+function apagarTema(n) {
+    console.log(n);
+    const tema = document.getElementById(`tema_${n}`);
+    tema.innerHTML = "";
+}
+
+var index2 = 1;
+
+function adicionarTarefa(){
+    const tarefa = input_tarefa.value;
+    const dataTarefa =  input_data_tarefa.value;
+    const integrante = input_integrante.value;
+
+    tarefas.innerHTML += `<p id="tarefa_${index2}">${tarefa} | ${dataTarefa.split('-').reverse().join('-')} | ${integrante} <button class="botao_apagar" onclick="apagarLinha(${index2})">x</button></p>`
+
+    index2++
+}
+
+function apagarLinha(n) {
+    console.log(n);
+    const linha = document.getElementById(`tarefa_${n}`);
+    linha.innerHTML = "";
+}
