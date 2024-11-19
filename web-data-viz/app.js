da27aa5b4391861565391c2d96d2940c4a11c6f9
AndreiScafiSPTech <andrei.vasconcelos@sptech.school>
2024-11-19 13:03:05 -0300

    Arrumando plotagem de dados com chartjs e integrando BobIA.

--- web-data-viz/app.js
+++ web-data-viz/app.js
@@ -35,6 +35,54 @@ app.use("/medidas", medidasRouter);
 app.use("/aquarios", aquariosRouter);
 app.use("/empresas", empresasRouter);
 
+//BobIA
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const chatIA = new GoogleGenerativeAI(process.env.MINHA_CHAVE);
+
+// rota para receber perguntas e gerar respostas
+app.post("/perguntar", async (req, res) => {
+    const pergunta = req.body.pergunta;
+
+    try {
+        const resultado = await gerarResposta(pergunta);
+        res.json({ resultado });
+    } catch (error) {
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+
+});
+
+
+// configurando o servidor para servir arquivos estáticos
+app.use(express.static(path.join(__dirname, "public")));
+
+// configurando CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+    next();
+});
+
+
+// função para gerar respostas usando o gemini
+async function gerarResposta(mensagem) {
+    // obtendo o modelo de IA
+    const modeloIA = chatIA.getGenerativeModel({ model: "gemini-pro" });
+
+    try {
+        // gerando conteúdo com base na pergunta
+        const resultado = await modeloIA.generateContent(`Em um paragráfo responda: ${mensagem}`);
+        const resposta = await resultado.response.text();
+
+        console.log(resposta);
+
+        return resposta;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
 app.listen(PORTA_APP, function () {
     console.log(`
     ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######  
