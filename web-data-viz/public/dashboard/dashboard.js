49295738a5e47ef2e80e09fb22b8f124303df7c9
AndreiScafiSPTech <andrei.vasconcelos@sptech.school>
2024-12-02 14:11:56 -0300

    Gabriel implementou a KPIs

--- web-data-viz/public/dashboard/dashboard.js
+++ web-data-viz/public/dashboard/dashboard.js
@@ -1,54 +1,31 @@
-// function obterTanque(idEmpresa){
-//     fetch(`/dashboard/obterTanque/${idEmpresa}`, {cache: 'no-store'}).then(function (resposta){
-//         if(resposta.ok){
-//             resposta.json().then(function (res){
-//                 plotarTanque(res)
-//             })
-//         }
-//     })
-// }
 
-// function plotarTanque(resposta){
-//     const containerTanque = document.getElementById('btnTanque')
 
-//     resposta.forEach(function (item) {
-//         containerTanque.innerHTML += `
-            
-//             <div class = "containerTanque" onclick = "obterKPI(${item.idTanque})">
-//                 <img class="iconeTanque"  src="../assets/icons/fermentacao (ok).png"/>
-//                 Tanque ${item.idTanque}</div>
-//         `
-//     });
-// }
+function obterKPI(idTanque, limite) {
 
-function obterKPI(idTanque, limite){
-    console.log("EI TO CHEGANDO AQUI COM O ID "+idTanque)
-    display_temperatura.innerText = ''
-                grafico_co2.innerText = ' '
-
-    fetch(`/dashboard/obterMinMaxTemperatura/${idTanque}`).then(function(resposta){
-        if(resposta.ok){
-            resposta.json().then(function (res){
+    fetch(`/dashboard/obterMinMaxTemperatura/${idTanque}`).then(function (resposta) {
+        if (resposta.ok) {
+            resposta.json().then(function (res) {
                 console.log(res)
-                display_temperaturaMin.innerText = res[0].minima
-                display_temperaturaMax.innerText = res[0].maxima
-                
+                document.getElementById('display_temperaturaMin').innerHTML = `<i class="fa-solid fa-arrow-down"></i> ${Number(res[0].minima)}°`
+                document.getElementById('display_temperaturaMax').innerHTML = `<i class="fa-solid fa-arrow-up"></i> ${Number(res[0].maxima)}°`
+
             })
         }
     })
 
-    fetch(`/dashboard/obterKPI/${idTanque}/${limite}`).then(function(resposta){
-        if(resposta.ok){
-            resposta.json().then(function (res){
-                console.log(res)      
-                    display_temperatura.innerText = res.Temperatura
-                    grafico_co2.innerText = res.co2        
-            }).catch(function(){
+    fetch(`/dashboard/obterKPI/${idTanque}/${limite}`).then(function (resposta) {
+
+        if (resposta.ok) {
+            resposta.json().then(function (res) {
+                console.log(res)
+                document.getElementById('display_temperatura').innerText = `${Number(res.Temperatura)}°`
+                grafico_co2.innerText = `${Number(res.co2)}%`
+            }).catch(function () {
                 display_temperatura.innerText = '0'
                 grafico_co2.innerText = '0'
             })
         }
-    }).catch(function(erro){
-        console.log('Erro ao exibir kpi '+ erro)
+    }).catch(function (erro) {
+        console.log('Erro ao exibir kpi ' + erro)
     })
 }
