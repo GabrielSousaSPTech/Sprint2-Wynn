cf5c6bc646f4a0500470c1814f2209abb00a06cd
mikki-unix <mikki.anjos@sptech.school>
2024-12-03 20:52:32 -0300

    Rota para adicionar tanques
    Agora é possível adicionar novos tanques através da tela de lista de tanques.
    
    Resumo de adições e/ou modificações
    * Ao carregar a página, é feito também a requisição de tipos de vinho. De acordo com esses dados, são acrescentadas options no selects.
    * A requisição de tanques é guardada no localStorage, e será usada para edição dos tanques.
    * Os novos tanques cadastrados funcionam diretamente para a Dash.

--- web-data-viz/public/JS/tanques.js
+++ web-data-viz/public/JS/tanques.js
@@ -1,9 +1,37 @@
-function obterDados() {
+function obterVinhos() {
+    fetch(`/tipoVinho/listar`).then(function (resposta) {
+        if (resposta.ok) {
+            resposta.json().then(function (res) {
+                preencherSelectTipos(res)
+            }).catch(function () {
+            })
+        }
+    }).catch(function (erro) {
+        console.log('Erro ao exibir kpi ' + erro)
+    })
+}
+
+function preencherSelectTipos(dados) {
+    var options = ''
+
+    dados.forEach(vinho => {
+        const id = vinho.idTipoVinho
+        const nome = vinho.nomeVinho
+
+        options += `<option value="${id}">${nome}</option>`
+    });
+
+    const selects = document.getElementsByName('select_tipo_vinho')
+    selects.forEach(select => {
+        select.innerHTML = options
+    })
+}
+
+function obterTanques() {
     fetch(`/tanque/listar/${sessionStorage.ID_USUARIO}`).then(function (resposta) {
         if (resposta.ok) {
             resposta.json().then(function (res) {
-                console.log('Tanques da empresa:')
-                console.log(res)
+                localStorage.tanques = JSON.stringify(res)
                 listarTanques(res)
             }).catch(function (err) {
                 console.log(err)
@@ -30,7 +58,7 @@ function listarTanques(dados) {
                 <td>${tipo}</td>
                 <td class="celula-status ${status}"><span>${statusFormatado}</span></td>
                 <td class="tabela-icone">
-                    <button class="botao-tabela" onclick="exibirOcultarMenu('editar', ${id})">
+                    <button class="botao-tabela" onclick="exibirOcultarMenu('editar', false, ${id - 1})">
                         <img src="../assets/icons/icon_gear.png">
                     </button>
                 </td>
@@ -42,4 +70,54 @@ function listarTanques(dados) {
             </tr>
         `
     });
+}
+
+var idTanqueEditar = undefined
+
+function exibirOcultarMenu(nomeMenu = '', voltar = false, pos = undefined) {
+    div_janela_tabela.style.display = div_janela_tabela.style.display == 'flex' ? 'none' : 'flex'
+
+    const menu = document.getElementById(`aside_menu_${nomeMenu}`)
+    menu.style.display = menu.style.display == 'none' ? 'flex' : 'none'
+
+    if (pos != undefined && nomeMenu == 'editar') {
+        const tanqueSelecionado = localStorage.tanques[pos]
+        const idSelecionado = tanqueSelecionado.id
+
+        idTanqueEditar = idSelecionado
+
+        span_id_tanque.innerText = idSelecionado
+        select_editar_tipo_vinho.value = tanqueSelecionado.fkTipo
+        select_editar_atividade.value = tanqueSelecionado.atv
+    }
+
+    if (voltar) {
+        idTanqueEditar = undefined
+        obterTanques()
+    }
+}
+
+function adicionarTanque() {
+    const idVinho = select_adicionar_tipo_vinho.value
+    const atvTanque = select_adicionar_atividade.value
+
+    fetch('/tanque/adicionar', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            idEmpresa: sessionStorage.ID_USUARIO,
+            idVinho: idVinho,
+            status: atvTanque
+        })
+    }).then(function (resposta) {
+        console.log(resposta)
+
+        if (resposta.ok) {
+            alert('Tanque adicionado com sucesso.')
+        }
+    }).catch(function (erro) {
+        console.log(erro)
+    })
 }
--- web-data-viz/src/models/tanqueModel.js
+++ web-data-viz/src/models/tanqueModel.js
@@ -1,21 +1,34 @@
 var database = require("../database/config")
 
 function obterTanquesEmpresa(idEmpresa) {
-    
-    var instrucaoSql = `
+
+    const instrucaoSql = `
         select
-	        idTanque as id,
+            idTanque as id,
             nomeVinho as tipo,
+            fkTipoVinho as fk,
             statusTanque as atv
-	        	from tbTanque
+                from tbTanque
                 join tbTipoVinho on fkTipoVinho = idTipoVinho
-                where fkEmpresa = ${idEmpresa};
+                    where fkEmpresa = 1
+                    order by idTanque;
     `;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
+function adicionarTanque(idEmpresa, idVinho, status) {
+    const instrucaoSql = `
+        INSERT INTO tbTanque (fkEmpresa, fkTipoVinho, statusTanque) VALUE 
+            (${idEmpresa}, ${idVinho}, '${status}');
+    `
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
 module.exports = {
-    obterTanquesEmpresa
+    obterTanquesEmpresa,
+    adicionarTanque
 };
